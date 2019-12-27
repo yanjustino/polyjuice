5cb89c3e1d24688f3234c5574e3c42abeccb2d39
Yan Justino <yan.justino@ivia.com.br>
2019-12-27 10:54:16 -0300

    Refactor Cnpj and Cpf Methods

--- Polyjuice/Potions/PtBr/Identification.cs
+++ Polyjuice/Potions/PtBr/Identification.cs
@@ -7,9 +7,9 @@ namespace Polyjuice.Potions.PtBr
     {
         public static string Rg => "#########".Numerify();
         public static string PrettyRg => "###.###.###".Numerify();
-        public static string Cpf => NumberGenerator.CpfSemMascara();        
+        public static string Cpf => NumberGenerator.CpfSemMascara();
         public static string PrettyCpf => NumberGenerator.CpfComMascara();
-        public static string Cnpj => NumberGenerator.CnpjSemMascara();        
+        public static string Cnpj => NumberGenerator.CnpjSemMascara();
         public static string PrettyCnpj => NumberGenerator.CnpjComMascara();
     }
 
@@ -48,53 +48,14 @@ namespace Polyjuice.Potions.PtBr
                     _error = 1;
                     do
                     {
-                        if (_error > 1)
-                        {
-                            //MessageBox.Show("Numero invalido.\n");
-                            _error = 1;
-                        }
-
+                        if (_error > 1) _error = 1;
                         _number[_i] = (_rand.Next()) % 9;
                         _error++;
                     } while (_number[_i] > 9 || _number[_i] < 0);
                 }
 
-                //*==========================================*
-                //|       Primeiro digito verificador        |
-                //*==========================================*
-                _sumOne = ((_number[1] * 10) +
-                         (_number[2] * 9) +
-                         (_number[3] * 8) +
-                         (_number[4] * 7) +
-                         (_number[5] * 6) +
-                         (_number[6] * 5) +
-                         (_number[7] * 4) +
-                         (_number[8] * 3) +
-                         (_number[9] * 2));
-                _partOne = (_sumOne / 11);
-                _parteTwo = (_partOne * 11);
-                _parteThree = (_sumOne - _parteTwo);
-                _dig1 = (11 - _parteThree);
-                if (_dig1 > 9) _dig1 = 0;
-                
-                //*==========================================*
-                //|        Segundo digito verificador        |
-                //*==========================================*
-                _sumTwo = ((_number[1] * 11) +
-                         (_number[2] * 10) +
-                         (_number[3] * 9) +
-                         (_number[4] * 8) +
-                         (_number[5] * 7) +
-                         (_number[6] * 6) +
-                         (_number[7] * 5) +
-                         (_number[8] * 4) +
-                         (_number[9] * 3) +
-                         (_dig1 * 2));
-                _parteFive = (_sumTwo / 11);
-                _parteSix = (_parteFive * 11);
-                _parteSeven = (_sumTwo - _parteSix);
-                _dig2 = (11 - _parteSeven);
-                if (_dig2 > 9) _dig2 = 0;
+                PrimeiroDigitoVerificadorCpf();
+                SegundoDigitoVerificadorCpf();
                 //*==========================================*
                 //|       Impressao do numero completo       | 
                 //*==========================================*
@@ -109,6 +70,44 @@ namespace Polyjuice.Potions.PtBr
             }
 
             return result;
+        }
+
+        private static void SegundoDigitoVerificadorCpf()
+        {
+            _sumTwo = ((_number[1] * 11) +
+                       (_number[2] * 10) +
+                       (_number[3] * 9) +
+                       (_number[4] * 8) +
+                       (_number[5] * 7) +
+                       (_number[6] * 6) +
+                       (_number[7] * 5) +
+                       (_number[8] * 4) +
+                       (_number[9] * 3) +
+                       (_dig1 * 2));
+            _parteFive = (_sumTwo / 11);
+            _parteSix = (_parteFive * 11);
+            _parteSeven = (_sumTwo - _parteSix);
+            _dig2 = (11 - _parteSeven);
+            if (_dig2 > 9) _dig2 = 0;
+        }
+
+        private static void PrimeiroDigitoVerificadorCpf()
+        {
+            _sumOne = ((_number[1] * 10) +
+                       (_number[2] * 9) +
+                       (_number[3] * 8) +
+                       (_number[4] * 7) +
+                       (_number[5] * 6) +
+                       (_number[6] * 5) +
+                       (_number[7] * 4) +
+                       (_number[8] * 3) +
+                       (_number[9] * 2));
+            _partOne = (_sumOne / 11);
+            _parteTwo = (_partOne * 11);
+            _parteThree = (_sumOne - _parteTwo);
+
+            _dig1 = (11 - _parteThree);
+            if (_dig1 > 9) _dig1 = 0;
         }
 
         public static string CpfSemMascara(int size = 1)
@@ -131,51 +130,16 @@ namespace Polyjuice.Potions.PtBr
                     } while (_number[_i] > 9 || _number[_i] < 0);
                 }
 
-                //*==========================================*
-                //|       Primeiro digito verificador        |
-                //*==========================================*
-                _sumOne = ((_number[1] * 10) +
-                         (_number[2] * 9) +
-                         (_number[3] * 8) +
-                         (_number[4] * 7) +
-                         (_number[5] * 6) +
-                         (_number[6] * 5) +
-                         (_number[7] * 4) +
-                         (_number[8] * 3) +
-                         (_number[9] * 2));
-                _partOne = (_sumOne / 11);
-                _parteTwo = (_partOne * 11);
-                _parteThree = (_sumOne - _parteTwo);
-                _dig1 = (11 - _parteThree);
-                if (_dig1 > 9) _dig1 = 0;
-                //*==========================================*
-                //|        Segundo digito verificador        |
-                //*==========================================*
-                _sumTwo = ((_number[1] * 11) +
-                         (_number[2] * 10) +
-                         (_number[3] * 9) +
-                         (_number[4] * 8) +
-                         (_number[5] * 7) +
-                         (_number[6] * 6) +
-                         (_number[7] * 5) +
-                         (_number[8] * 4) +
-                         (_number[9] * 3) +
-                         (_dig1 * 2));
-                _parteFive = (_sumTwo / 11);
-                _parteSix = (_parteFive * 11);
-                _parteSeven = (_sumTwo - _parteSix);
-                _dig2 = (11 - _parteSeven);
-                if (_dig2 > 9) _dig2 = 0;
+                PrimeiroDigitoVerificadorCpf();
+                SegundoDigitoVerificadorCpf();
                 //*==========================================*
                 //|       Impressao do numero completo       | 
                 //*==========================================*
                 for (_i = 1; _i <= 9; _i++)
                 {
-                    //numeros do CPF
                     result += Convert.ToString(_number[_i]);
                 }
 
-                // dois últimos digitos
                 result += _dig1 + "" + _dig2 + Environment.NewLine;
             }
 
@@ -196,54 +160,13 @@ namespace Polyjuice.Potions.PtBr
                 _number[10] = 0;
                 _number[11] = 0;
                 _number[12] = (_rand.Next()) % 9;
-                //*==========================================*
-                //|       Primeiro digito verificador        |
-                //*==========================================*
-                _sumOne = ((_number[1] * 5) +
-                         (_number[2] * 4) +
-                         (_number[3] * 3) +
-                         (_number[4] * 2) +
-                         (_number[5] * 9) +
-                         (_number[6] * 8) +
-                         (_number[7] * 7) +
-                         (_number[8] * 6) +
-                         (_number[9] * 5) +
-                         (_number[10] * 4) +
-                         (_number[11] * 3) +
-                         (_number[12] * 2));
-                _partOne = (_sumOne / 11);
-                _parteTwo = (_partOne * 11);
-                _parteThree = (_sumOne - _parteTwo);
-                _dig1 = (11 - _parteThree);
-                if (_dig1 > 9) _dig1 = 0;
-                //*==========================================*
-                //|        Segundo digito verificador        |
-                //*==========================================*
-                _sumTwo = ((_number[1] * 6) +
-                         (_number[2] * 5) +
-                         (_number[3] * 4) +
-                         (_number[4] * 3) +
-                         (_number[5] * 2) +
-                         (_number[6] * 9) +
-                         (_number[7] * 8) +
-                         (_number[8] * 7) +
-                         (_number[9] * 6) +
-                         (_number[10] * 5) +
-                         (_number[11] * 4) +
-                         (_number[12] * 3) +
-                         (_dig1 * 2));
-                _parteFive = (_sumTwo / 11);
-                _parteSix = (_parteFive * 11);
-                _parteSeven = (_sumTwo - _parteSix);
-                _dig2 = (11 - _parteSeven);
-                if (_dig2 > 9) _dig2 = 0;
+                PrimeiroDigitoVerificadorCnpj();
+                SegundoDigitoVerificadorCnpj();
                 //*==========================================*
                 //|       Impressao do numero completo       | 
                 //*==========================================*
-                //ostrm.SetLength(0);
                 for (_i = 1; _i <= 12; _i++)
                 {
-                    //numeros do CNPJ
                     result += Convert.ToString(_number[_i]);
                     if (_i == 2) result += "."; //imprime um ponto depois da 2ª casa
                     if (_i == 5) result += "."; //imprime um ponto depois da 5ª casa
@@ -254,6 +177,49 @@ namespace Polyjuice.Potions.PtBr
             }
 
             return result;
+        }
+
+        private static void SegundoDigitoVerificadorCnpj()
+        {
+            _sumTwo = ((_number[1] * 6) +
+                       (_number[2] * 5) +
+                       (_number[3] * 4) +
+                       (_number[4] * 3) +
+                       (_number[5] * 2) +
+                       (_number[6] * 9) +
+                       (_number[7] * 8) +
+                       (_number[8] * 7) +
+                       (_number[9] * 6) +
+                       (_number[10] * 5) +
+                       (_number[11] * 4) +
+                       (_number[12] * 3) +
+                       (_dig1 * 2));
+            _parteFive = (_sumTwo / 11);
+            _parteSix = (_parteFive * 11);
+            _parteSeven = (_sumTwo - _parteSix);
+            _dig2 = (11 - _parteSeven);
+            if (_dig2 > 9) _dig2 = 0;
+        }
+
+        private static void PrimeiroDigitoVerificadorCnpj()
+        {
+            _sumOne = ((_number[1] * 5) +
+                       (_number[2] * 4) +
+                       (_number[3] * 3) +
+                       (_number[4] * 2) +
+                       (_number[5] * 9) +
+                       (_number[6] * 8) +
+                       (_number[7] * 7) +
+                       (_number[8] * 6) +
+                       (_number[9] * 5) +
+                       (_number[10] * 4) +
+                       (_number[11] * 3) +
+                       (_number[12] * 2));
+            _partOne = (_sumOne / 11);
+            _parteTwo = (_partOne * 11);
+            _parteThree = (_sumOne - _parteTwo);
+            _dig1 = (11 - _parteThree);
+            if (_dig1 > 9) _dig1 = 0;
         }
 
         public static string CnpjSemMascara(int size = 1)
@@ -270,58 +236,17 @@ namespace Polyjuice.Potions.PtBr
                 _number[10] = 0;
                 _number[11] = 0;
                 _number[12] = (_rand.Next()) % 9;
-                //*==========================================*
-                //|       Primeiro digito verificador        |
-                //*==========================================*
-                _sumOne = ((_number[1] * 5) +
-                         (_number[2] * 4) +
-                         (_number[3] * 3) +
-                         (_number[4] * 2) +
-                         (_number[5] * 9) +
-                         (_number[6] * 8) +
-                         (_number[7] * 7) +
-                         (_number[8] * 6) +
-                         (_number[9] * 5) +
-                         (_number[10] * 4) +
-                         (_number[11] * 3) +
-                         (_number[12] * 2));
-                _partOne = (_sumOne / 11);
-                _parteTwo = (_partOne * 11);
-                _parteThree = (_sumOne - _parteTwo);
-                _dig1 = (11 - _parteThree);
-                if (_dig1 > 9) _dig1 = 0;
-                //*==========================================*
-                //|        Segundo digito verificador        |
-                //*==========================================*
-                _sumTwo = ((_number[1] * 6) +
-                         (_number[2] * 5) +
-                         (_number[3] * 4) +
-                         (_number[4] * 3) +
-                         (_number[5] * 2) +
-                         (_number[6] * 9) +
-                         (_number[7] * 8) +
-                         (_number[8] * 7) +
-                         (_number[9] * 6) +
-                         (_number[10] * 5) +
-                         (_number[11] * 4) +
-                         (_number[12] * 3) +
-                         (_dig1 * 2));
-                _parteFive = (_sumTwo / 11);
-                _parteSix = (_parteFive * 11);
-                _parteSeven = (_sumTwo - _parteSix);
-                _dig2 = (11 - _parteSeven);
-                if (_dig2 > 9) _dig2 = 0;
+                
+                PrimeiroDigitoVerificadorCnpj();
+                SegundoDigitoVerificadorCnpj();
                 //*==========================================*
                 //|       Impressao do numero completo       | 
                 //*==========================================*
                 //ostrm.SetLength(0);
                 for (_i = 1; _i <= 12; _i++)
                 {
-                    //numeros do CNPJ
                     result += Convert.ToString(_number[_i]);
                 }
-
-                // dois últimos digitos
                 result += _dig1 + "" + _dig2 + Environment.NewLine;
             }
 
