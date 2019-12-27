a6b05046603bb1e23030438a576ea714d47ebaae
Yan Justino <yan.justino@ivia.com.br>
2019-12-27 10:41:34 -0300

    Removing Data From Models

--- Polyjuice/Potions/Address.cs
+++ Polyjuice/Potions/Address.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using Polyjuice.Extension;
+using Polyjuice.Potions.Data;
 
 namespace Polyjuice.Potions
 {
@@ -8,8 +9,8 @@ namespace Polyjuice.Potions
     /// </summary>
     public static class Address
     {
-        public static string State => Data.States.Rand();
-        public static string StateAbbr => Data.StatesAbbr.Rand();
+        public static string State => AddressData.States.Rand();
+        public static string StateAbbr => AddressData.StatesAbbr.Rand();
         public static string ZipCode => "#####-####".Numerify();
         public static string BuildingNumber => "### #####".Rand().Numerify();
         public static string Location => $"{BuildingNumber} {StreetName}";
@@ -21,9 +22,9 @@ namespace Polyjuice.Potions
                 switch (2.Randomize())
                 {
                     case 0:
-                        return $"{Name.LastName} {Data.StreetSufix.Rand()}";
+                        return $"{Name.LastName} {AddressData.StreetSufix.Rand()}";
                     case 1:
-                        return $"{Name.FirstName} {Data.StreetSufix.Rand()}";
+                        return $"{Name.FirstName} {AddressData.StreetSufix.Rand()}";
                     default:
                         return string.Empty;
                 }
@@ -34,109 +35,22 @@ namespace Polyjuice.Potions
         {
             get
             {
-                var prefix = Data.CompassDirections.Union(Data.CityPrefixes).ToArray();
+                var prefix = AddressData.CompassDirections.Union(AddressData.CityPrefixes).ToArray();
                 
                 switch (4.Randomize())
                 {
                     case 0:
                         return $"{prefix.Rand()} {Name.FirstName}";
                     case 1:
-                        return $"{prefix.Rand()} {Name.FirstName} {Data.CitySufix.Rand()}";
+                        return $"{prefix.Rand()} {Name.FirstName} {AddressData.CitySufix.Rand()}";
                     case 2:
-                        return $"{Name.FirstName} {Data.CitySufix.Rand()}";
+                        return $"{Name.FirstName} {AddressData.CitySufix.Rand()}";
                     case 3:
-                        return $"{Name.LastName} {Data.CitySufix.Rand()}";
+                        return $"{Name.LastName} {AddressData.CitySufix.Rand()}";
                     default:
                         return string.Empty;
                 }
             }
-        }
-
-        private static class Data
-        {
-            public static string[] StatesAbbr { get; } =
-            {
-                "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI",
-                "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV",
-                "NH",
-                "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA",
-                "WA",
-                "WV", "WI", "WY"
-            };
-
-            public static string[] States { get; } =
-            {
-                "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
-                "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
-                "Kentucky",
-                "Louisiana",
-                "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana",
-                "Nebraska", "Nevada",
-                "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
-                "Oklahoma", "Oregon",
-                "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah",
-                "Vermont",
-                "Virginia",
-                "Washington", "West Virginia", "Wisconsin", "Wyoming"
-            };
-
-            public static string[] CompassDirections { get; } =
-            {
-                "North", "East", "West", "South"
-            };
-
-            public static string[] CityPrefixes { get; } =
-            {
-                "New", "Lake", "Port"
-            };
-
-            public static string[] CitySufix { get; } =
-            {
-                "town", "ton", "land", "ville", "berg", "burgh", "borough", "bury", "view", "port",
-                "mouth", "stad", "furt", "chester", "mouth", "fort", "haven", "side", "shire"
-            };
-
-            public static string[] StreetSufix { get; } =
-            {
-                "Alley", "Avenue", "Branch", "Bridge", "Brook", "Brooks", "Burg", "Burgs", "Bypass", "Camp",
-                "Canyon", "Cape", "Causeway", "Center", "Centers", "Circle", "Circles", "Cliff", "Cliffs", "Club",
-                "Common",
-                "Corner",
-                "Corners", "Course", "Court", "Courts", "Cove", "Coves", "Creek", "Crescent", "Crest", "Crossing",
-                "Crossroad", "Curve",
-                "Dale", "Dam", "Divide", "Drive", "Drives", "Estate", "Estates", "Expressway", "Extension",
-                "Extensions",
-                "Fall", "Falls",
-                "Ferry", "Field", "Fields", "Flat", "Flats", "Ford", "Fords", "Forest", "Forge", "Forges", "Fork",
-                "Forks",
-                "Fort",
-                "Freeway", "Garden", "Gardens", "Gateway", "Glen", "Glens", "Green", "Greens", "Grove", "Groves",
-                "Harbor",
-                "Harbors",
-                "Haven", "Heights", "Highway", "Hill", "Hills", "Hollow", "Inlet", "Island", "Islands", "Isle",
-                "Junction",
-                "Junctions",
-                "Key", "Keys", "Knoll", "Knolls", "Lake", "Lakes", "Land", "Landing", "Lane", "Light", "Lights", "Loaf",
-                "Lock", "Locks",
-                "Lodge", "Loop", "Mall", "Manor", "Manors", "Meadow", "Meadows", "Mews", "Mill", "Mills", "Mission",
-                "Motorway", "Mount",
-                "Mountain", "Mountains", "Neck", "Orchard", "Oval", "Overpass", "Park", "Parks", "Parkway", "Parkways",
-                "Pass", "Passage",
-                "Path", "Pike", "Pine", "Pines", "Place", "Plain", "Plains", "Plaza", "Point", "Points", "Port",
-                "Ports",
-                "Prairie",
-                "Radial", "Ramp", "Ranch", "Rapid", "Rapids", "Rest", "Ridge", "Ridges", "River", "Road", "Roads",
-                "Route",
-                "Row",
-                "Rue", "Run", "Shoal", "Shoals", "Shore", "Shores", "Skyway", "Spring", "Springs", "Spur", "Spurs",
-                "Square", "Squares",
-                "Station", "Stravenue", "Stream", "Street", "Streets", "Summit", "Terrace", "Throughway", "Trace",
-                "Track",
-                "Trafficway",
-                "Trail", "Tunnel", "Turnpike", "Underpass", "Union", "Unions", "Valley", "Valleys", "Via", "Viaduct",
-                "View", "Views",
-                "Village", "Villages", "Ville", "Vista", "Walk", "Walks", "Wall", "Way", "Ways", "Well", "Wells"
-            };
         }
     }
 }
---
+++ Polyjuice/Potions/Data/AddressData.cs
@@ -0,0 +1,89 @@
+namespace Polyjuice.Potions.Data
+{
+    public static class AddressData
+    {
+        public static string[] StatesAbbr { get; } =
+        {
+            "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI",
+            "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV",
+            "NH",
+            "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA",
+            "WA",
+            "WV", "WI", "WY"
+        };
+
+        public static string[] States { get; } =
+        {
+            "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
+            "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
+            "Kentucky",
+            "Louisiana",
+            "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana",
+            "Nebraska", "Nevada",
+            "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+            "Oklahoma", "Oregon",
+            "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah",
+            "Vermont",
+            "Virginia",
+            "Washington", "West Virginia", "Wisconsin", "Wyoming"
+        };
+
+        public static string[] CompassDirections { get; } =
+        {
+            "North", "East", "West", "South"
+        };
+
+        public static string[] CityPrefixes { get; } =
+        {
+            "New", "Lake", "Port"
+        };
+
+        public static string[] CitySufix { get; } =
+        {
+            "town", "ton", "land", "ville", "berg", "burgh", "borough", "bury", "view", "port",
+            "mouth", "stad", "furt", "chester", "mouth", "fort", "haven", "side", "shire"
+        };
+
+        public static string[] StreetSufix { get; } =
+        {
+            "Alley", "Avenue", "Branch", "Bridge", "Brook", "Brooks", "Burg", "Burgs", "Bypass", "Camp",
+            "Canyon", "Cape", "Causeway", "Center", "Centers", "Circle", "Circles", "Cliff", "Cliffs", "Club",
+            "Common",
+            "Corner",
+            "Corners", "Course", "Court", "Courts", "Cove", "Coves", "Creek", "Crescent", "Crest", "Crossing",
+            "Crossroad", "Curve",
+            "Dale", "Dam", "Divide", "Drive", "Drives", "Estate", "Estates", "Expressway", "Extension",
+            "Extensions",
+            "Fall", "Falls",
+            "Ferry", "Field", "Fields", "Flat", "Flats", "Ford", "Fords", "Forest", "Forge", "Forges", "Fork",
+            "Forks",
+            "Fort",
+            "Freeway", "Garden", "Gardens", "Gateway", "Glen", "Glens", "Green", "Greens", "Grove", "Groves",
+            "Harbor",
+            "Harbors",
+            "Haven", "Heights", "Highway", "Hill", "Hills", "Hollow", "Inlet", "Island", "Islands", "Isle",
+            "Junction",
+            "Junctions",
+            "Key", "Keys", "Knoll", "Knolls", "Lake", "Lakes", "Land", "Landing", "Lane", "Light", "Lights", "Loaf",
+            "Lock", "Locks",
+            "Lodge", "Loop", "Mall", "Manor", "Manors", "Meadow", "Meadows", "Mews", "Mill", "Mills", "Mission",
+            "Motorway", "Mount",
+            "Mountain", "Mountains", "Neck", "Orchard", "Oval", "Overpass", "Park", "Parks", "Parkway", "Parkways",
+            "Pass", "Passage",
+            "Path", "Pike", "Pine", "Pines", "Place", "Plain", "Plains", "Plaza", "Point", "Points", "Port",
+            "Ports",
+            "Prairie",
+            "Radial", "Ramp", "Ranch", "Rapid", "Rapids", "Rest", "Ridge", "Ridges", "River", "Road", "Roads",
+            "Route",
+            "Row",
+            "Rue", "Run", "Shoal", "Shoals", "Shore", "Shores", "Skyway", "Spring", "Springs", "Spur", "Spurs",
+            "Square", "Squares",
+            "Station", "Stravenue", "Stream", "Street", "Streets", "Summit", "Terrace", "Throughway", "Trace",
+            "Track",
+            "Trafficway",
+            "Trail", "Tunnel", "Turnpike", "Underpass", "Union", "Unions", "Valley", "Valleys", "Via", "Viaduct",
+            "View", "Views",
+            "Village", "Villages", "Ville", "Vista", "Walk", "Walks", "Wall", "Way", "Ways", "Well", "Wells"
+        };
+    }
+}
