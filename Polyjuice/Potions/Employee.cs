6d0023fa76a315ff8353d893b0c685bc3a7fa744
leisiamedeiros <leisiamedeiros@gmail.com>
2021-05-24 21:02:51 -0300

    Create employees class

---
+++ Polyjuice/Potions/Employee.cs
@@ -0,0 +1,18 @@
+using Polyjuice.Extension;
+using static Polyjuice.Potions.Data.DataReader;
+using GlobalCompany = Polyjuice.Potions.Company;
+
+namespace Polyjuice.Potions
+{
+    /// <summary>
+    /// An employee that works for an organization
+    /// </summary>
+    public static class Employee
+    {
+        public static string Username => Name.FirstName;
+        public static string FullName => $"{Username} {Name.LastName}";
+        public static string Enrollment => "########".Numerify();
+        public static string Company => GlobalCompany.CompanyName;
+        public static string Role => $"{Read("Job", "JobNouns").Rand()}";
+    }
+}
